e53dc2732db934373b73d89282f549762b652afd
victor muñoz manzo <victor90.mz2@gmail.com>
2016-07-19 17:16:30 -0500

    nvos css para la seccion de tu evento

--- Drupal/icyPotions/templates/page/include/header.php
+++ Drupal/icyPotions/templates/page/include/header.php
@@ -1,26 +1,26 @@
-<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">
+<div id="jssor_1" style="width: 1300px; height: 500px;">
 					<!-- Loading Screen -->
-					<div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-						<div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-						<div style="position:absolute;display:block;background:url('/sites/default/files/scroll/img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+					<div class="loading-slide" data-u="loading">
+						<div class="loading-slide-o"></div>
+						<div class="loading-slide-i"></div>
 					</div>
-					<div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
+					<div id="slides-jssor" data-u="slides">
 						<div data-p="225.00" style="display: none;">
-							<!--img data-u="image" src="/sites/default/files/scroll/img/red.jpg" /-->
-							<div style="position: absolute; top: 94px; left: 635px; width: 480px; height: 120px; font-size: 50px; color: #ffffff; line-height: 60px;">TOUCH SWIPE SLIDER</div>
-							<div style="position: absolute; top: 283px; left: 708px; width: 480px; height: 120px; font-size: 30px; color: #ffffff; line-height: 38px;">Build your slider with anything, includes image, content, text, html, photo, picture</div>
-							<div data-u="caption" data-t="0" style="position: absolute; top: 85px; left: 155px; width: 450px; height: 350px;">
-								<img style="position: absolute; top: 0px; left: 0px; width: 450px; height: 350px;" src="/sites/default/files/scroll/img/c-phone.png" />
-								<img data-u="caption" data-t="1" style="position: absolute; top: 70px; left: 130px; width: 102px; height: 78px;" src="/sites/default/files/scroll/img/c-jssor-slider.png" />
-								<img data-u="caption" data-t="2" style="position: absolute; top: 153px; left: 163px; width: 80px; height: 53px;" src="/sites/default/files/scroll/img/c-text.png" />
-								<img data-u="caption" data-t="3" style="position: absolute; top: 60px; left: 220px; width: 140px; height: 90px;" src="/sites/default/files/scroll/img/c-fruit.png" />
-								<img data-u="caption" data-t="4" style="position: absolute; top: -123px; left: 121px; width: 200px; height: 155px;" src="/sites/default/files/scroll/img/c-navigator.png" />
+							<div id="div-img"><img src="/sites/default/files/scroll/title-home.png" /></div>
+							<div id="dcaption">
+								<h2>A nuestro </br><strong>Mundo Congelado</strong></br><strong class="name-site">Icy Potions</strong></h2>
+								<p>¡Por que no todas las paletas de hielo tienen que ser </p><p>iguales, nosotros decidimos hacerlas divertidas!</p>
+								<p><strong>¡Creamos los mejores, más populares y más </strong></p><p><strong>novedosos cocteles y los hacemos paletas de hielo!</strong></p>
 							</div>
-							<div data-u="caption" data-t="5" style="position: absolute; top: 142px; left: 231px; width: 470px; height: 220px;">
-								<div style="position: absolute; top: 4px; left: 45px; width: 379px; height: 213px; overflow: hidden;">
-									<img data-u="caption" data-t="6" style="position: absolute; top: -2px; left: 8px; width: 379px; height: 213px;" src="/sites/default/files/scroll/img/c-slide-3.jpg" />
-									<img data-u="caption" data-t="7" style="position: absolute; top: 0px; left: 379px; width: 379px; height: 213px;" src="/sites/default/files/scroll/img/c-slide-1.jpg" />
-								</div>
+							<div id="simages" data-u="caption" data-t="0">
+								<img src="/sites/default/files/scroll/1hielos.png" />
+								<img id="secimage" data-u="caption" data-t="1" src="/sites/default/files/scroll/2-kiwis-moras.png" />
+								<img id="terimage" data-u="caption" data-t="2" src="/sites/default/files/scroll/3-vodka.png" />
+								<img id="cuimage" data-u="caption" data-t="3" src="/sites/default/files/scroll/paleta.png" />
+								<img id="quimage"data-u="caption" data-t="4" src="/sites/default/files/scroll/fresas.png" />
+							</div>
+							<div id="finalimage" data-u="caption" data-t="5">
+								<div><img src="/sites/default/files/scroll/final.png" /></div>
 							</div>
 						</div>
 						<div data-p="225.00" style="display: none;">
@@ -29,15 +29,14 @@
 						<div data-p="225.00" data-po="80% 55%" style="display: none;">
 							<img data-u="image" src="/sites/default/files/scroll/img/blue.jpg" />
 						</div>
-						<a data-u="add" href="http://www.jssor.com" style="display:none">Jssor Slider</a>
 					
 					</div>
 					<!-- Bullet Navigator -->
-					<div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
+					<div id="dnavigator" data-u="navigator" class="jssorb05" data-autocenter="1">
 						<!-- bullet navigator item prototype -->
-						<div data-u="prototype" style="width:16px;height:16px;"></div>
+						<div data-u="prototype"></div>
 					</div>
 					<!-- Arrow Navigator -->
-					<span data-u="arrowleft" class="jssora22l" style="top:0px;left:12px;width:40px;height:58px;" data-autocenter="2"></span>
-					<span data-u="arrowright" class="jssora22r" style="top:0px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
-				</div>
+					<span data-u="arrowleft" class="jssora22l" id="aleft" data-autocenter="2"></span>
+					<span data-u="arrowright" class="jssora22r" id="aright" data-autocenter="2"></span>
+</div>
